9fef13222a83c25673f1c338d3ceb6df8855c495
Yousaf Butt <yousaf.butt@mobylogix.net>
2023-01-20 00:10:30 +0500

    user api updated

--- pages/api/users.js
+++ pages/api/users.js
@@ -17,11 +17,10 @@ export default async function handler (req, res) {
         res.status(400).json({ success: false })
       }
       break
-      case 'POST':
-        try {
-          const user = await User.create(req.body)
-          res.status(201).json({ success: true, data: user })
-          console.log(users);
+    case 'POST':
+      try {
+        const user = await User.create(req.body)
+        res.status(201).json({ success: true, data: user })
       } catch (error) {
         res.status(400).json({ success: false })
       }
